6175e060f652045bf220a927b23d2e9a9d1a1e2f
Carlos Andres Arroyo Espinoza <74364155+carlos-andres99@users.noreply.github.com>
2021-10-16 02:31:24 -0400

    Update Program.cs

--- Ej3/Program.cs
+++ Ej3/Program.cs
@@ -6,11 +6,18 @@ namespace Ej3
     {
         static void Main(string[] args)
         {
-            Verdura lechuga=new Verdura("lechuga",6,3,1);
-            Fruta platano=new Fruta("plátano",5,4,3,3);
-            Carne cerdo=new Carne("Cerdo",20,4,3,"carne roja");
-            Console.WriteLine(lechuga.verifyP(platano));
-
+            Verdura lechuga=new Verdura("lechuga",6,2,1);
+            Fruta platano=new Fruta("plátano",5,2,3,3);
+            Carne cerdo=new Carne("Cerdo",20,2,3,"carne roja");
+            lechuga.show();
+            platano.show();
+            cerdo.show();
+            Console.WriteLine("tienen las mismas vitaminas: ");
+            Console.WriteLine(lechuga.verifyV(platano));
+            Console.WriteLine("Tienen el mismo precio:");
+            Console.WriteLine(platano.verifyP(lechuga));
+            Console.WriteLine("minerales en común:");
+            cerdo.minerals(lechuga);
         }
     }
 }
